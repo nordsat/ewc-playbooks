e5b637b0c3fde57ca2305b4c251fd6a62c299b53
Martin Raspaud <martin.raspaud@smhi.se>
2026-04-15 12:18:20 +0200

    Fix viirs wms and perf

--- viirs-wms/templates/init.sql
+++ viirs-wms/templates/init.sql
@@ -1,1 +1,3 @@
 CREATE TABLE {{ pg_table_name }} (id serial primary key, filename varchar, product_name varchar, time timestamp, geom geometry);
+CREATE INDEX {{ pg_table_name }}_geom_idx ON {{ pg_table_name }} USING GIST (geom);
+CREATE INDEX {{ pg_table_name }}_product_time_idx ON {{ pg_table_name }} (product_name, time DESC);
